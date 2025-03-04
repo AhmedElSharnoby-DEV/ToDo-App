c6ee2d0a3dc5fcfa29aa794a5f583e4ef38e29df
ahmedElsharnouby <ahmed.elsharnouby@asset.com.eg>
2025-03-04 11:52:26 +0200

    feature: update card style

--- src/modules/MealProject/Components/MealList.jsx
+++ src/modules/MealProject/Components/MealList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import MealCard from './MealCard';
+import '../Styles/MealList.css';
 
 const MealList = () => {
   const [mealItems, setMealItems] = useState([]);
@@ -14,11 +15,11 @@ const MealList = () => {
     fetchTodos();
   }, []);
   return (
-    <>
+    <div className="gridContainer">
       {mealItems.map((item) => {
         return <MealCard key={item.idMeal} mealDetails={item} />;
       })}
-    </>
+    </div>
   );
 };
 
